fd471a4980125ed36c67973783fe365be54c6192
tigreq <56633076+tigreq@users.noreply.github.com>
2021-03-31 20:39:04 +0500

    Update app.f465c45f.js

--- js/app.f465c45f.js
+++ js/app.f465c45f.js
@@ -454,13 +454,29 @@
                     staticClass: "column-flex"
                 }, [e("div", {
                     staticClass: "text"
-                }, [t._v(" Для участия в школе дронов подавайте заявку ")]), e("br"), e("v-btn", {
+                }, [t._v(" Для участия в школе дронов подавайте заявку на главной странице сайта")]), e("br"), e("v-btn", {
                     attrs: {
                         rounded: "",
                         color: "#223880",
                         dark: ""
                     }
-                }, [t._v(" Подать заявку ")])], 1)
+					
+					
+					e("a", {
+                    attrs: {
+                        href: "https://yevgenevna.github.io/"
+                    }
+                
+					
+					
+					
+					
+					
+					
+					
+					
+					
+                }, [t._v(" Вернуться на главную ")])], 1)
             },
             lt = [];
         console.log("PartialsDronComponent", I);
